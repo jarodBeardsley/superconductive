381bf7c7d9e6b09c99d3f4802fe6effac8db3406
jarodBeardsley <jarod.beardsley@gmail.com>
2021-06-28 18:59:23 -0400

    Streamlining
    Removed redundant import and improved exception-handling logic

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 """Driver script for the Superconductive technical challenge
-
 author: Jarod Beardsley
 version: 06/28/2021
 """
-import astropy
-import astropy.coordinates
+import astropy.coordinates as ap
 from astropy.time import Time
 import requests
 
+
 def solar_longitude(time):
-    """Returns the solar longitude coordinates, given a UTC time"""
-    try:
-        return astropy.coordinates.get_sun(time).to_string().split(" ")[0]
-    except Exception as e:
-        print("Something went wrong: " + str(e))
+    """Returns the solar longitude coordinates, given a UTC time in ISO format"""
+    return ap.get_sun(time).to_string().split(" ")[0]
 
 
 if __name__ == '__main__':
-    times = requests.get("http://127.0.0.1:5000/").text.split(",")
-    if len(times) < 2:
-        t = Time(times[0], format='isot', scale='utc')
-        print("solar longitude at " + times[0] + " is: " + solar_longitude(t) + " degrees.")
-    else:
-        print("More than one time detected, please input only one time per call.")
+    try:
+        times = requests.get("http://127.0.0.1:5000/").text.split(",")
+        if len(times) < 2:
+            t = Time(times[0], format='iso', scale='utc')
+            print("Solar longitude at " + str(t) + " is: " + solar_longitude(t) + " degrees.")
+        else:
+            print("More than one time detected, please provide only one time per call.")
+    except Exception as e:
+        print("Something went wrong: " + str(e))
