72d27c6d8e1691414cf6fd7dd3a929b4081edc8d
jarodBeardsley <jarod.beardsley@gmail.com>
2021-06-28 17:43:33 -0400

    Add documentation

--- time_app.py
+++ time_app.py
@@ -1,7 +1,13 @@
+"""Defines a REST endpoint with Flask that returns a UTC time
+
+author: Jarod Beardsley
+version: 06/28/2021
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def time():
+    """Returns a UTC time"""
     return "1999-01-01T00:00:00.123456789"
